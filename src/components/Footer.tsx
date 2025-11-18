233f2a7dc0fceb56458db004c7d61d0829e25787
kidde60 <kgeorgewilliam60@gmail.com>
2025-11-19 00:20:48 +0300

    Add header

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer: React.FC = () => {
+  return (
+    <div className="text-center py-8 text-sm" style={{ color: "#AFAFAF" }}>
+      <span>clinicPesa </span>
+      <a
+        href="#"
+        className="font-semibold underline hover:opacity-80 transition-opacity"
+        style={{ color: "#013E5D" }}
+      >
+        T&Cs
+      </a>
+      <span> apply.</span>
+    </div>
+  );
+};
+
+export default Footer;
